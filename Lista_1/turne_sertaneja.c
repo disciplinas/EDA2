0e4571f6bc4bc17e60e6b990d81ac7d96155d071
Luciano dos Santos Silva <e941008@corp.caixa.gov.br>
2019-08-27 12:34:19 -0300

    Problema da turne sertaneja adicionado

---
+++ Lista_1/turne_sertaneja.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct qnode{
+    char key[26];
+    struct qnode* next;
+};
+
+struct queue {
+    struct qnode *first, *last;
+};
+
+struct qnode* newNode(char *k){
+    struct qnode* temp = (struct qnode*)malloc(sizeof(struct qnode));
+    strcpy(temp->key, k);
+    //temp->key = k;
+    temp->next = NULL;
+    return temp;
+};
+
+struct queue* createQueue(){
+    struct queue* q = (struct queue*)malloc(sizeof(struct queue));
+    q->first = q->last = NULL;
+    return q;
+}
+
+void enqueue(struct queue* q, char *k){
+    struct qnode* temp = newNode(k);
+
+    if(q->last == NULL){
+        q->first = q->last = temp;
+        return;
+    }
+
+    q->last->next = temp;
+    q->last = temp;
+}
+
+struct qnode* dequeue(struct queue* q){
+    if(q->first == NULL){
+        return NULL;
+    }
+
+    struct qnode* temp = q->first;
+    q->first = q->first->next;
+
+    if(q->first == NULL){
+        q->last == NULL;
+    }
+
+    return temp;
+
+}
+
+
+
+
+
+int main(){
+    struct queue* q = createQueue();
+    enqueue(q, "Brasilia");
+
+    struct qnode* n = dequeue(q);
+    printf("%s", n->key);
+
+    return 0;
+}
