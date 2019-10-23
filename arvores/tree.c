853995dffe3b025818987a80a5a651feb73d444e
Luciano Santos <luciano_z7@hotmail.com>
2019-10-23 14:44:21 -0300

    Código de árvore binária

--- arvores/tree.c
+++ arvores/tree.c
@@ -7,7 +7,102 @@ typedef struct no{
     struct no *dir;
 } tNo;
 
+typedef tNo *tArvBin;
 
+void cria(tArvBin *T){
+    *T = NULL;
+}
+
+int vazia(tArvBin T){
+    return(T==NULL);
+}
+
+tArvBin busca(tArvBin T, int dado){
+    tArvBin achou;
+    if(T==NULL){
+        return NULL;
+    }
+
+    if(T->conteudo == dado){
+        return T;
+    }
+
+    achou = busca(T->esq, dado);
+    if(achou == NULL){
+        achou = busca(T->dir,dado);
+    }
+    return achou;
+}
+
+int insereRaiz(tArvBin *T, int dado){
+    tNo *novoNo;
+    if(*T != NULL){
+        return (0);
+    }
+
+    novoNo = malloc(sizeof(tNO));
+    if(novoNo == NULL){
+        return 0;
+    }
+
+    novoNo->conteudo = dado;
+    novoNo->esq = NULL;
+    novoNo->dir = NULL;
+    *T = novoNo;
+    return 1;
+}
+
+int insereDireita(tArvBin T, int vPai, int vFilho){
+    tNo *f, *p, *novoNo;
+
+    f = busca(T, vFilho);
+    if(f != NULL){
+        return 0;
+    }
+
+    p = busca(T, vPai);
+    if(p == NULL)
+        return 0;
+    if(p->dir != NULL){
+        return 0;
+    }
+    novoNo = malloc(sizeof(tNo));
+    if(novoNo == NULL){
+        return (0);
+    }
+
+    novoNo->conteudo = vFilho;
+    novoNo->esq = NULL;
+    novoNo->dir = NULL;
+    p->dir = novoNo;
+    return 1;
+}
+
+int insereEsquerda(tArvBin T, int vPai, int vFilho){
+    tNo *f, *p, *novoNo;
+
+    f = busca(T, vFilho);
+    if(f != NULL){
+        return 0;
+    }
+
+    p = busca(T, vPai);
+    if(p == NULL)
+        return 0;
+    if(p->esq != NULL){
+        return 0;
+    }
+    novoNo = malloc(sizeof(tNo));
+    if(novoNo == NULL){
+        return (0);
+    }
+
+    novoNo->conteudo = vFilho;
+    novoNo->esq = NULL;
+    novoNo->dir = NULL;
+    p->esq = novoNo;
+    return 1;
+}
 
 int main(){
 
